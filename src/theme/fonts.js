f61f1cc800fb697d115ec2ae67daa002e1248399
Antonin Nhek <a.nhek@icloud.com>
2021-09-16 14:13:58 +0200

    Initial commit from Create Next App

---
+++ src/theme/fonts.js
@@ -0,0 +1,7 @@
+const fonts = {
+  // add fonts from https://fontsource.org
+  heading: 'sans-serif',
+  body: 'sans-serif',
+}
+
+export default fonts
---
+++ src/theme/global.js
@@ -0,0 +1,10 @@
+const global = {
+  // styles for the `body`
+  body: {
+    fontSize: '16px',
+    fontFamily: 'body',
+    bg: 'white',
+  },
+}
+
+export default global
